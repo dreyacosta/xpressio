ecfd5dd4963f3f12f18095915613497f742893c9
cybermarkus1 <cybermarkus1@email.com>
2013-12-02 16:01:22 +0100

    keeping oopacity bg

---
+++ javascripts/xpressio.js
@@ -0,0 +1,82 @@
+(function() {
+  'use strict';
+
+  var root = this;
+
+  var xpressio = {};
+
+  if (typeof exports !== 'undefined') {
+    xpressio = exports;
+  } else {
+    xpressio = root.xpressio = {};
+  }
+
+  xpressio.VERSION = '0.3.5';
+
+  document.addEventListener('click', function(e) {
+    e.stopPropagation();
+
+    var dropdowns = document.querySelectorAll('.dropdown .menu');
+
+    for (var i = 0; i < dropdowns.length; i++) {
+      var dropdown = dropdowns[i];
+
+      dropdown.classList.remove('visible');
+    }
+  });
+
+  var modals = xpressio.modals = function() {
+    var modalsAll = document.querySelectorAll('[data-modal]');
+
+    var openModal = function(event) {
+      var element = event.currentTarget;
+      var modalId = element.getAttribute('data-modal');
+      var thisModal = document.querySelector('[data-modal-id="' + modalId + '"');
+      var overlay = document.querySelector('.modal-overlay');
+
+      overlay.addEventListener('click', closeModal);
+
+      overlay.classList.add('modal-show');
+      thisModal.classList.add('modal-show');
+    }
+
+    var closeModal = function() {
+      var modalsAllId = document.querySelectorAll('[data-modal-id]');
+      var overlay = document.querySelector('.modal-overlay');
+
+      for (var i = 0; i < modalsAllId.length; i++) {
+        var modal = modalsAllId[i];
+
+        modal.classList.remove('modal-show');
+      }
+
+      overlay.classList.remove('modal-show');
+    }
+
+    for (var i = 0; i < modalsAll.length; i++) {
+      var modal = modalsAll[i];
+
+      modal.addEventListener('click', openModal);
+    }
+  }
+
+  var dropdown = xpressio.dropdown = function() {
+    var dropdowns = document.querySelectorAll('.dropdown');
+
+    var openMenu = function(e) {
+      e.stopPropagation();
+      this.querySelector('.menu').classList.toggle('visible');
+    }
+
+    for (var i = 0; i < dropdowns.length; i++) {
+      var dropdown = dropdowns[i];
+
+      dropdown.addEventListener('click', openMenu);
+    }
+  }
+
+  modals();
+  dropdown();
+
+  return xpressio;
+}).call(this);
