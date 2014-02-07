f3f291ed10fa30c92a50af5091eb5f46fdf88cc2
David Rey <david.rey.acosta@gmail.com>
2014-02-07 15:47:08 +0100

    Added JS to copy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,6 +35,7 @@ module.exports = function(grunt) {
               '*.html',
               '*.ico',
               'stylesheets/*.css',
+              'javascripts/*.js',
               'font/*'
             ],
             dest: 'www/'
